df9643d468d13eab008b371cbf85ad34697cde83
David Demory <david.demory@gmail.com>
2019-07-31 14:37:20 -0400

    2d commit

--- Model_fitting/Host/fun_host.m
+++ Model_fitting/Host/fun_host.m
@@ -2,7 +2,7 @@ function ymod=fun_host(data,theta)
 
 % starting concentrations are at the end of the parameter vector
 y0 = theta(end);
-
+%y0 = data.ydata(1,2);
 % time is the first column of data.ydata
 t  = data.ydata(:,1);
 
